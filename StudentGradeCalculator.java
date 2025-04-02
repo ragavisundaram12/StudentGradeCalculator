2dffde519319efeb27e748f193437a48284253c0
Ragavi <ragavi9003@mountzion.ac.in>
2025-04-02 13:57:58 +0530

    first commit

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int subjects = scanner.nextInt();
+        int[] marks = new int[subjects];
+        int total = 0;
+
+        for (int i = 0; i < subjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            total += marks[i];
+        }
+
+        double percentage = (double) total / subjects;
+        char grade;
+
+        if (percentage >= 90) grade = 'A';
+        else if (percentage >= 80) grade = 'B';
+        else if (percentage >= 70) grade = 'C';
+        else if (percentage >= 60) grade = 'D';
+        else grade = 'F';
+
+        System.out.println("\nTotal Marks: " + total);
+        System.out.println("Percentage: " + percentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
